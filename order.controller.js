9c2148a79e121e3b89c0cb7b436e07a77a2a0b00
Naresh Kumar H <naresh.20201012@gmail.com>
2021-02-12 18:25:11 +0530

    Added api for addOrder

--- order.controller.js
+++ order.controller.js
@@ -1,61 +1,60 @@
 const { OrderService } = require("./order.service");
 
 class OrderController {
-    constructor() {
-        this.orderService = new OrderService()
-    }
+  constructor() {
+    this.orderService = new OrderService();
+  }
 
-    async allOrders(req, res) {
-        try {
-            let orders = await this.orderService.getAllOrder();
-            res.json(orders);
-        } catch (err) {
-            res.setStatus(404).json({ message: err.message });
-        }
+  async allOrders(req, res) {
+    try {
+      let orders = await OrderService.getAllOrder();
+      res.json(orders);
+    } catch (err) {
+      res.status(404).json({ message: err.message });
     }
+  }
 
-    async myOrders(req, res) {
-        let userId = req.query.userId;
-        try {
-            let myOrders = await this.orderService.getMyOrder(userId);
-            res.json(myOrders);
-        } catch (err) {
-            res.setStatus(404).json({ message: err.message });
-
-        }
+  async myOrders(req, res) {
+    let userId = req.query.userId;
+    try {
+      let myOrders = await OrderService.getMyOrder(userId);
+      res.json(myOrders);
+    } catch (err) {
+      res.status(404).json({ message: err.message });
     }
+  }
 
-    async changOrderStatus(req, res) {
-        let orderId = req.query.orderId;
-        let status = req.query.status;
-        try {
-            let order = await this.orderService.changeOrderStatus(orderId, status);
-            res.json(order)
-        } catch (err) {
-            res.setStatus(404).json({ message: err.message });
-        }
+  async changOrderStatus(req, res) {
+    let orderId = req.query.orderId;
+    let status = req.query.status;
+    try {
+      let order = await OrderService.changeOrderStatus(orderId, status);
+      res.json(order);
+    } catch (err) {
+      res.status(404).json({ message: err.message });
     }
+  }
 
-    async addOrder(req, res) {
-        try {
-            let orderDetails = req.body;
-            // console.log(orderDetails);
-            let orderedProduct = await this.orderService.addOrder(orderDetails);
-            res.json(orderedProduct);
-        } catch (err) {
-            res.setStatus(400).json({ message: err.message });
-        }
+  async addOrder(req, res) {
+    try {
+      let orderDetails = req.body;
+      // console.log(orderDetails);
+      let orderedProduct = await OrderService.addOrder(orderDetails);
+      res.json(orderedProduct);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
     }
+  }
 
-    async cancelOrder(req, res) {
-        try {
-            let orderId = req.query.orderId;
-            let order = await this.orderService.cancelOrder(orderId);
-            res.json(order);
-        } catch (err) {
-            res.setStatus(404).json({ message: err.message });
-        }
+  async cancelOrder(req, res) {
+    try {
+      let orderId = req.query.orderId;
+      let order = await OrderService.cancelOrder(orderId);
+      res.json(order);
+    } catch (err) {
+      res.status(404).json({ message: err.message });
     }
+  }
 }
 
 exports.OrderController = OrderController;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { OrderController } = require("./order.controller");
 const { ProductController } = require("./product.controller");
 const { UserController } = require("./user.controller");
 const app = express();
@@ -7,8 +8,10 @@ const port = process.env.PORT || 3000;
 
 const userController = new UserController();
 const productController = new ProductController();
+const orderController = new OrderController();
 app.get("/", (req, res) => res.send("REST API- v2"));
 app.post("/userLogin", userController.loginUser);
 app.post("/addUser", userController.newUserRegistration);
 app.get("/activeproducts", productController.getActiveProductsList);
+app.post("/addOrder", orderController.addOrder);
 app.listen(port, () => console.log(` app listening on port ${port}!`));
