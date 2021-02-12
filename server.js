0cb04a9220e000311500ae6af212571eca27be45
Naresh Kumar H <naresh.20201012@gmail.com>
2021-02-12 16:36:46 +0530

    Updated port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.send("REST API"));
 app.listen(port, () => console.log(` app listening on port ${port}!`));
