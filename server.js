e3db3100a15392de6d6f16e569b57cbd186c65e6
Naresh Kumar H <naresh.20201012@gmail.com>
2021-02-12 16:57:35 +0530

    Added login feature

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const { UserController } = require("./user.controller");
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
+const userController = new UserController();
 app.get("/", (req, res) => res.send("REST API- v2"));
+app.post("/userLogin", userController.loginUser);
 app.listen(port, () => console.log(` app listening on port ${port}!`));
