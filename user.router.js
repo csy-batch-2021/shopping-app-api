463db9760a4e216605750fa96b33f50ea010696e
Guru2824 <78194137+Guru2824@users.noreply.github.com>
2021-02-12 10:32:11 +0530

    Add files via upload

--- user.router.js
+++ user.router.js
@@ -3,7 +3,7 @@ const { Response } = require("./response");
 
 const userController = new UserController();
 
-async function sendRequest(url, method) {
+async function sendRequest(url, method, req, res) {
     if (url == "users") {
         if (method == "GET") {
             await userController.getUsersList(req, res);
@@ -20,25 +20,9 @@ async function sendRequest(url, method) {
         if (method == "POST") {
             await userController.newUserRegistration(req, res);
         }
+    } else if (url == "loginUser") {
+        if (method == "GET") {
+            await userController.loginUser(req, res);
+        }
     }
 }
-
-const res = new Response();
-let body = {};
-let req = { body: body, query: {} };
-
-// let results = sendRequest("users", "Get", req, res);
-
-// req.body = { userId: 10, status: 0 };
-let results = sendRequest("getActiveUsers", "GET", req, res);
-
-// req.query.userId = 11;
-// let results = sendRequest("getUser", "Get", req, res);
-
-// req.body = { name: "kumar", email: "kumar@gmail.com", password: "chainsys", role: "" }
-// let results = sendRequest("addUser", "POST", req, res);
-
-results.then(data => {
-    // console.table(res);
-    console.log(res.toString());
-});
