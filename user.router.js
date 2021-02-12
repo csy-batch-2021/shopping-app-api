d0fa3642d9426faaa6532af382aa2d63fecac9d6
Guru2824 <78194137+Guru2824@users.noreply.github.com>
2021-02-12 10:47:46 +0530

    Add files via upload

--- user.router.js
+++ user.router.js
@@ -3,26 +3,31 @@ const { Response } = require("./response");
 
 const userController = new UserController();
 
-async function sendRequest(url, method, req, res) {
-    if (url == "users") {
-        if (method == "GET") {
-            await userController.getUsersList(req, res);
-        }
-    } else if (url == "getActiveUsers") {
-        if (method == "GET") {
-            await userController.getAllactiveUsers(req, res);
-        }
-    } else if (url == "getUser") {
-        if (method == "GET") {
-            await userController.findOneUser(req, res);
-        }
-    } else if (url == "addUser") {
-        if (method == "POST") {
-            await userController.newUserRegistration(req, res);
-        }
-    } else if (url == "loginUser") {
-        if (method == "GET") {
-            await userController.loginUser(req, res);
+class UserRouter {
+
+    async  sendRequest(url, method, req, res) {
+        if (url == "users") {
+            if (method == "GET") {
+                await userController.getUsersList(req, res);
+            }
+        } else if (url == "getActiveUsers") {
+            if (method == "GET") {
+                await userController.getAllactiveUsers(req, res);
+            }
+        } else if (url == "getUser") {
+            if (method == "GET") {
+                await userController.findOneUser(req, res);
+            }
+        } else if (url == "addUser") {
+            if (method == "POST") {
+                await userController.newUserRegistration(req, res);
+            }
+        } else if (url == "loginUser") {
+            if (method == "GET") {
+                await userController.loginUser(req, res);
+            }
         }
     }
 }
+
+exports.UserRouter = UserRouter;
