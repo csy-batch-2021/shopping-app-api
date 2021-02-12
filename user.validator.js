3504ef22aeb67bdd69444926285f8961ba7c48a4
Guru2824 <78194137+Guru2824@users.noreply.github.com>
2021-02-12 14:16:44 +0530

    Add files via upload

--- user.validator.js
+++ user.validator.js
@@ -6,22 +6,25 @@ class UserValidator {
 
     static async validateNewUser(user) {
         if (user.name == null || user.name == "" || user.name.trim() == 0) {
-            throw new Error("Name Invalid");
+            throw new Error("Name cannot be empty");
         }
         else if (user.email == null || user.email == "" || user.email.trim() == 0) {
-            throw new Error("Email Invalid");
+            throw new Error("Email cannot be empty");
         }
         else if (user.password == null || user.password == "" || user.password.trim() == 0) {
-            throw new Error("Password Invalid");
+            throw new Error("Password cannot be empty");
+        }
+        else if (user.password.length < 8) {
+            throw new Error("password length should be least 8 characters");
         }
     }
 
     static async isvalidEmail(login) {
         if (login.email == null || login.email == "" || login.email.trim() == 0) {
-            throw new Error("Email Invalid");
+            throw new Error("Email cannot be empty");
         }
         else if (login.password == null || login.password == "" || login.password.trim() == 0) {
-            throw new Error("Password Invalid");
+            throw new Error("Password cannot be empty");
         }
     }
 
