dce782dc2c7a5b3e554460510254536682593267
Naresh Kumar H <nareshkumarh@live.com>
2021-02-15 11:50:54 +0530

    Added dotenv

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-    host: "65.1.119.59",
-    port: 3306,
-    user: "dharani",
-    password: "Dharani@2021",
-    database: "dharani_db",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     connectionLimit: 1
 });
 
