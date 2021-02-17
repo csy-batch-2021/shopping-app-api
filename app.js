7efda40dabda9b88df79bcd5e64824a573e382c3
Naresh Kumar H <naresh.20201012@gmail.com>
2021-02-17 18:39:20 +0530

    Added API

--- app.js
+++ app.js
@@ -1,50 +1,11 @@
-const { OrderController } = require("./order.controller");
-const { ProductController } = require("./product.controller");
-const { UserController } = require("./user.controller");
-const { UserValidator } = require("./user.validator");
-
 const express = require("express");
 const router = express.Router();
 
-const userController = new UserController();
-const productController = new ProductController();
-const orderController = new OrderController();
+router.get("/", (req, res) => {
+  console.log("Default");
+  res.send("REST API- v2");
+});
+/*
 
-router.get("/", (req, res) => res.send("REST API- v2"));
-
-// User Details
-router.post("/userLogin", userController.loginUser);
-router.post("/addUser", userController.newUserRegistration);
-router.get("/UsersList", userController.getUsersList);
-router.patch(
-  "/changeUserStatus",
-  UserValidator.isAdmin,
-  userController.updateUserStatus
-);
-
-router.patch(
-  "/addBalance",
-  UserValidator.isAdmin,
-  userController.updateBalance
-);
-
-// Products
-router.get("/activeproducts", productController.getActiveProductsList);
-router.post("/searchProducts", productController.searchProducts);
-router.post("/addNewProducts", productController.addNewProducts);
-router.get("/getOneProductDetail", productController.getOneProductDetails);
-router.patch("/changeProductStatus", productController.changeProductStatus);
-router.get("/getAllProducts", productController.getProducts);
-
-// Orders
-router.post("/addOrder", orderController.addOrder);
-router.get("/myorders", orderController.myOrders);
-router.get("/orders", orderController.allOrders);
-router.post(
-  "/changeorderstatus",
-  UserValidator.isAdmin,
-  orderController.changOrderStatus
-);
-router.post("/cancelorder", orderController.cancelOrder);
-
+*/
 module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,62 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv-flow").config();
 
+const { OrderController } = require("./order.controller");
+const { ProductController } = require("./product.controller");
+const { UserController } = require("./user.controller");
+const { UserValidator } = require("./user.validator");
+
+const userController = new UserController();
+const productController = new ProductController();
+const orderController = new OrderController();
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 // Create Routes
 const apiRoutes = require("./app");
-app.get("/api", apiRoutes);
+//console.log(apiRoutes);
+
+// User Details
+app.post("/userLogin", userController.loginUser);
+app.post("/addUser", userController.newUserRegistration);
+app.get("/UsersList", userController.getUsersList);
+app.patch(
+  "/changeUserStatus",
+  UserValidator.isAdmin,
+  userController.updateUserStatus
+);
+
+app.patch("/addBalance", UserValidator.isAdmin, userController.updateBalance);
+
+// Products
+app.get("/api/activeproducts", productController.getActiveProductsList);
+app.post("/api/searchProducts", productController.searchProducts);
+app.post("/api/addNewProducts", productController.addNewProducts);
+app.get("/api/getOneProductDetail", productController.getOneProductDetails);
+app.patch("/api/changeProductStatus", productController.changeProductStatus);
+app.get("/api/getAllProducts", productController.getProducts);
+
+// Orders
+app.post("/api/addOrder", orderController.addOrder);
+app.get("/api/myorders", orderController.myOrders);
+app.get("/api/orders", orderController.allOrders);
+app.post(
+  "/api/changeorderstatus",
+  UserValidator.isAdmin,
+  orderController.changOrderStatus
+);
+app.post("/api/cancelorder", orderController.cancelOrder);
+
+app.get("/", (req, res) => {
+  res.send("Success");
+});
+// Create Commmon Error Handler
+app.use(function (err, req, res, next) {
+  console.log("common error handler");
+  console.error(err);
+  res.status(500).json({ errorMessage: err.message });
+});
 
 const port = process.env.PORT || 3000;
 console.log(process.env.DB_HOST);
