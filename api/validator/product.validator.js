62489906431b6c884f156bf56c2b168edf2ccd3b
Dharaneeshwaran97 <58076976+Dharaneeshwaran97@users.noreply.github.com>
2021-02-22 16:18:21 +0530

    Add files via upload

--- api/validator/product.validator.js
+++ api/validator/product.validator.js
@@ -1,14 +1,24 @@
 const { ProductDAO } = require("../dao/product.dao");
+const { UserValidator } = require("../validator/user.validator");
 class ProductValidator {
   static validateNewProduct(product) {
     if (product.name == null || product.name.trim().length == 0) {
       throw new Error("please enter validate Name");
-    } else if (
+    } else if (UserValidator.nameValidator(product.name)) {
+      throw err;
+
+    }
+
+    else if (
       product.brandName == null ||
       product.brandName.trim().length == 0
     ) {
       throw new Error("please enter validate BrandName ");
-    } else if (product.ram == null || product.ram <= 0) {
+    } else if (UserValidator.nameValidator(product.brandName)) {
+      throw err;
+
+    }
+    else if (product.ram == null || product.ram <= 0) {
       throw new Error("please enter validate Ram");
     } else if (product.price == null || product.price <= 0) {
       throw new Error("please enter validate Price");
