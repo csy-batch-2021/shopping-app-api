1d46cbfde524559765657b1c203268bf62d9adce
Guru2824 <78194137+Guru2824@users.noreply.github.com>
2021-02-22 16:58:05 +0530

    Add files via upload

--- api/validator/user.validator.js
+++ api/validator/user.validator.js
@@ -2,6 +2,7 @@
 const { UserDAO } = require("../dao/user.dao");
 const bcrypt = require("bcrypt");
 const userDAO = new UserDAO();
+const validator = require("email-validator");
 
 class UserValidator {
   static async validateStatusInput(userId, status) {
@@ -15,68 +16,86 @@ class UserValidator {
     }
   }
 
-  static async balanceValidator1(bals, id) {
+  static async balanceValidator(bals, id) {
     if (id == null || id == "" || id.trim() == 0) {
       throw new Error("UserId cannot be empty");
-    } else if (bals == null || bals == "" || bals.trim() == 0) {
-      throw new Error("Balance cannot be empty");
     } else if (isNaN(id)) {
       throw new Error("UserId is not a number");
-    }
-  }
-
-  static async validateNewUser(user) {
-    if (user.name == null || user.name == "" || user.name.trim() == 0) {
-      throw new Error("Name cannot be empty");
-    } else if (user.name.length < 3) {
-      throw new Error("Name should be at least 3 characters");
-    } else if (
-      user.email == null ||
-      user.email == "" ||
-      user.email.trim() == 0
-    ) {
-      throw new Error("Email cannot be empty");
-    } else if (
-      user.password == null ||
-      user.password == "" ||
-      user.password.trim() == 0
-    ) {
-      throw new Error("Password cannot be empty");
-    } else if (user.password.length < 8) {
-      throw new Error("password length should be at least 8 characters");
-    }
-  }
-
-  static async balanceValidator(bals, id) {
-    if (bals <= 0) {
+    } else if (bals == null || bals == "" || bals.trim() == 0) {
+      throw new Error("Balance cannot be empty");
+    } else if (bals <= 0) {
       throw new Error("Invalid Balance Amount");
-    }
-    else if (bals >= 90000) {
+    } else if (bals > 90000) {
       throw new Error("Add Below RS.90000")
     }
   }
 
-  static async passwordValidator(password) {
-    let regularExpression = /^[a-zA-Z0-9]+$/;
-    let valid = regularExpression.test(password);
-    if (valid == false) {
+  // static async balanceValidator(bals, id) {
+  //   if (bals <= 0) {
+  //     throw new Error("Invalid Balance Amount");
+  //   }
+  //   else if (bals >= 90000) {
+  //     throw new Error("Add Below RS.90000")
+  //   }
+  // }
+
+
+  static async validateNewUser(user) {
+    if (user.name == null || user.name == "") {
+      throw new Error("Name cannot be empty");
+    } else if (user.name.length < 3) {
+      throw new Error("Name should be at least 3 characters");
+    }
+    else if (user.name.trim() == 0) {
+      throw new Error("Name Must Contain Characters Only");
+    }
+    let regularExpression = /^[a-zA-Z]+$/;
+    let nameValid = regularExpression.test(user.name);
+    if (nameValid == false) {
+      throw new Error("Name Must Contain Characters Only");
+    }
+    else if (user.email == null || user.email == "" || user.email.trim() == 0) {
+      throw new Error("Email cannot be empty");
+    }
+    let emailVal = validator.validate(user.email);
+    if (emailVal == false) {
+      throw new Error("Invalid Email Format");
+    }
+    else if (user.password == null || user.password == "" || user.password.trim() == 0) {
+      throw new Error("Password cannot be empty");
+    } else if (user.password.length < 8) {
+      throw new Error("password length should be at least 8 characters");
+    }
+    let regularExpressionPassword = /^[a-zA-Z0-9]+$/;
+    let passwordValid = regularExpressionPassword.test(user.password);
+    if (passwordValid == false) {
       throw new Error("Password Should Contain Alphanumeric Characters Only");
     }
   }
 
-  static async nameValidator(name) {
-    var regularExpression = /^[a-zA-Z]+$/;
-    var valid = regularExpression.test(name);
-    if (valid == false) {
-      throw new Error("Name Must Contain Characters Only");
-    }
-  }
 
-  static async emailValidator(email) {
-    if (email == false) {
-      throw new Error("Invalid Email Format");
-    }
-  }
+  // static async passwordValidator(password) {
+  //   let regularExpression = /^[a-zA-Z0-9]+$/;
+  //   let valid = regularExpression.test(password);
+  //   if (valid == false) {
+  //     throw new Error("Password Should Contain Alphanumeric Characters Only");
+  //   }
+  // }
+
+  // static async nameValidator(name) {
+  //   var regularExpression = /^[a-zA-Z]+$/;
+  //   var valid = regularExpression.test(name);
+  //   if (valid == false) {
+  //     throw new Error("Name Must Contain Characters Only");
+  //   }
+  // }
+
+  // static async emailValidator(email) {
+  //   let emailVal = validator.validate(email);
+  //   if (emailVal == false) {
+  //     throw new Error("Invalid Email Format");
+  //   }
+  // }
 
   static async isEmailExists(usersList) {
     if (usersList == 0) {
@@ -95,28 +114,33 @@ class UserValidator {
   }
 
   static async isvalidEmail(login) {
-    if (login.email == null || login.email == "" || login.email.trim() == 0) {
+    if (login.email == null || login.email == "") {
       throw new Error("Email cannot be empty");
-    } else if (
-      login.password == null ||
-      login.password == "" ||
-      login.password.trim() == 0
-    ) {
+    }
+    else if (login.email.trim() == 0) {
+      throw new Error("Invalid Email Format");
+    }
+    let emailVal = validator.validate(login.email);
+    if (emailVal == false) {
+      throw new Error("Invalid Email Format");
+    }
+    else if (login.password == null || login.password == "" || login.password.trim() == 0) {
       throw new Error("Password cannot be empty");
     }
   }
 
   static async updatePasswordValid(oldPassword, newPassword) {
-    if (newPassword == null || newPassword == "" || newPassword.trim() == 0) {
-      throw new Error("New Password cannot be empty");
-    } else if (
-      oldPassword == null ||
-      oldPassword == "" ||
-      oldPassword.trim() == 0
-    ) {
+    if (oldPassword == null || oldPassword == "" || oldPassword.trim() == 0) {
       throw new Error("oldPassword cannot be empty");
+    } else if (newPassword == null || newPassword == "" || newPassword.trim() == 0) {
+      throw new Error("New Password cannot be empty");
     } else if (newPassword.length < 8) {
       throw new Error("password length should be at least 8 characters");
+    }
+    let regularExpressionPassword = /^[a-zA-Z0-9]+$/;
+    let passwordValid = regularExpressionPassword.test(newPassword);
+    if (passwordValid == false) {
+      throw new Error("New Password Should Contain Alphanumeric Characters Only");
     }
     // else if (userId == null || userId == '') {
     //     throw new Error("ID cannot be empty");
