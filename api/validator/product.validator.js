5ef741dd877eba1bb89eac52f6174dd2449b3c39
Dharaneeshwaran97 <58076976+Dharaneeshwaran97@users.noreply.github.com>
2021-02-22 19:18:03 +0530

    changes product service

--- api/validator/product.validator.js
+++ api/validator/product.validator.js
@@ -3,20 +3,13 @@ const { UserValidator } = require("../validator/user.validator");
 class ProductValidator {
   static validateNewProduct(product) {
     if (product.name == null || product.name.trim().length == 0) {
-      throw new Error("please enter validate Name");
-    } else if (UserValidator.nameValidator(product.name)) {
-      throw err;
-
+      throw new Error("please enter validate Product Name");
     }
-
     else if (
       product.brandName == null ||
       product.brandName.trim().length == 0
     ) {
       throw new Error("please enter validate BrandName ");
-    } else if (UserValidator.nameValidator(product.brandName)) {
-      throw err;
-
     }
     else if (product.ram == null || product.ram <= 0) {
       throw new Error("please enter validate Ram");
